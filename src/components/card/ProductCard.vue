c64f287dd635e1453853b4b179575c01a43917f2
Alexandro B S <jornalsinal@gmail.com>
2026-03-19 02:27:12 -0300

    algumas pequenas correções em ProductCard

--- src/components/card/ProductCard.vue
+++ src/components/card/ProductCard.vue
@@ -4,29 +4,32 @@
       <img :src="'https://picsum.photos/200'" class="w-full object-cover" />
     </template>
     <template #title>
-      <h1>{{ product?.title }}</h1> </template
-    >;
+      <h1>{{ product?.title }}</h1>
+    </template>
     <template #content>
       <p>{{ product?.description }}</p>
       <p>Preço: R$ {{ product?.price.toFixed(2).replace('.', ',') }}</p>
     </template>
     <template #footer>
       <div class="flex flex-row-reverse">
-        <Button label="Adicionar ao carrinho" @click="addToCart(product!)" />
+        <Button label="Adicionar ao carrinho" @click="addToCart(product)" />
       </div>
     </template>
   </Card>
 </template>
+
 <script lang="ts">
 import type { Product } from '@/model/product.model'
 import { defineComponent, type PropType } from 'vue'
+
 export default defineComponent({
   props: {
     product: {
       type: Object as PropType<Product>,
+      required: true,
     },
   },
-  emits: { onclick },
+  emits: ['onclick'],
   methods: {
     addToCart(product: Product) {
       this.$emit('onclick', product)
